f4722848ff540194f712ad89d58d6e0052ab6d42
Amit Pawar <pawaramitaaa@gmail.com>
2020-07-17 00:21:55 +0530

    ADDED:calculate employee daily wage.

--- EmployeeWageCompitation.sh
+++ EmployeeWageCompitation.sh
@@ -13,4 +13,16 @@ echo $employ
 fi
 
 #.....................................................................................
+if [ "$employ" == "Present" ]
+then
+Employwageperhour=20
+worktime=8
+dailyemploywage=$(($Employwageperhour * $worktime))
+echo $dailyemploywage
+else
+dailyemploywage=0
+echo $dailyemploywage
+fi
+
+#.................................................................................
 
