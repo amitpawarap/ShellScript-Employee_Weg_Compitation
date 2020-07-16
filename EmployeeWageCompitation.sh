e4e70d27a26e79be462cc4fc3415f55a06f2c785
Amit Pawar <pawaramitaaa@gmail.com>
2020-07-17 00:15:48 +0530

    ADDED:check employee present or absent

--- EmployeeWageCompitation.sh
+++ EmployeeWageCompitation.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
 echo "welcome to Employee Wage Computation"
+#............................................................................
+
+Attendence=$(( $RANDOM % 2 + 1 ))
+if [ $Attendence -eq 1 ]
+then
+employ="Present"
+echo $employ
+else
+employ="Absent"
+echo $employ
+fi
+
+#.....................................................................................
+
